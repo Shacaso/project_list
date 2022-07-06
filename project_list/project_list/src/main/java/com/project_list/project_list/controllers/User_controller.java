88d7e494430fc7c44711eda9524bfd119d5f2c5e
Araya Alejo <alejoaraya2000@gmail.com>
2022-07-06 00:59:25 -0300

    configurate ide

--- project_list/project_list/src/main/java/com/project_list/project_list/controllers/User_controller.java
+++ project_list/project_list/src/main/java/com/project_list/project_list/controllers/User_controller.java
@@ -14,27 +14,28 @@ public class User_controller {
     private User_DAO user_dao;
 
     @RequestMapping(value = "api/users", method = RequestMethod.GET)
-    public List<User> Get_users(){
+    public List<User> Get_users () {
         return user_dao.Get_users();
     }
 
-    @RequestMapping(value = "api/users/{id}",method = RequestMethod.DELETE)
-    public void Delete_users(@PathVariable Long id){
-        user_dao.delete_user(id);
+    @RequestMapping(value = "api/users/{id}", method = RequestMethod.DELETE)
+    public void Delete_users ( @PathVariable Long id ) {
+        user_dao.delete_user( id );
     }
 
     @RequestMapping(value = "api/users", method = RequestMethod.POST)
-    public void Add_users(@RequestBody User user){
-        user_dao.register_user(user);
+    public void Add_users ( @RequestBody User user ) {
+        user_dao.register_user( user );
     }
+
     @RequestMapping(value = "user1", method = RequestMethod.GET)
-    public List<User> Edit_users(){
+    public List<User> Edit_users () {
         return List.of();//PASS
     }
 
     @RequestMapping(value = "api/login", method = RequestMethod.POST)
-    public String login(@RequestBody User user){
-        if(user_dao.Auth_user(user)){
+    public String login ( @RequestBody User user ) {
+        if ( user_dao.Auth_user( user ) ) {
             return "OK";
         } else {
             return "FAIL";
--- project_list/project_list/src/main/java/com/project_list/project_list/models/User.java
+++ project_list/project_list/src/main/java/com/project_list/project_list/models/User.java
@@ -8,23 +8,26 @@ import javax.persistence.*;
 @Table(name = "users")
 @AllArgsConstructor //LOMBOK
 @NoArgsConstructor //LOMBOK
+@Getter
+@Setter
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Getter @Setter @Column (name = "id")
+
+    @Column(name = "id")
     private Long ID;
 
-    @Getter @Setter @Column (name = "first_name")
+    @Column(name = "first_name")
     private String first_name;
 
-    @Getter @Setter @Column (name = "last_name")
+    @Column(name = "last_name")
     private String last_name;
 
-    @Getter @Setter @Column (name = "mail")
+    @Column(name = "mail")
     private String mail;
 
-    @Getter @Setter @Column (name = "password")
+    @Column(name = "password")
     private String password;
 
 }
